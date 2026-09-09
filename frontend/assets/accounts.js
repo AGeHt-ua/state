9ef95f5096ed4fdf42456d4e390d6feef51ed8da
AGeHt-ua <d.f.12339@gmail.com>
2026-09-09 23:09:53 +0300

    Include passport details in account definitions
    Added passport information for each account.

--- frontend/assets/accounts.js
+++ frontend/assets/accounts.js
@@ -1,7 +1,7 @@
 window.STATE_ACCOUNTS = [
-  { login: "castro", password: "castro", name: "Dominic Castro", roles: ["governor"], post: "Губернатор штату Сан-Андреас" },
-  { login: "admin", password: "state-admin", name: "Адміністратор порталу", roles: ["governor"], post: "Адміністратор порталу" },
-  { login: "doj", password: "doj", name: "Attorney General", roles: ["prosecutor"], post: "Генеральний прокурор" },
-  { login: "dept", password: "dept", name: "Директор департаменту", roles: ["official"], post: "Директор департаменту" },
-  { login: "court", password: "court", name: "Голова Верховного Суду", roles: ["court"], post: "Голова Верховного Суду" }
+  { login: "castro", password: "castro", name: "Dominic Castro", roles: ["governor"], post: "Губернатор штату Сан-Андреас", passport: "12279" },
+  { login: "admin", password: "state-admin", name: "Адміністратор порталу", roles: ["governor"], post: "Адміністратор порталу", passport: "10001" },
+  { login: "doj", password: "doj", name: "Attorney General", roles: ["prosecutor"], post: "Генеральний прокурор", passport: "20002" },
+  { login: "dept", password: "dept", name: "Директор департаменту", roles: ["official"], post: "Директор департаменту", passport: "30003" },
+  { login: "court", password: "court", name: "Голова Верховного Суду", roles: ["court"], post: "Голова Верховного Суду", passport: "40004" }
 ];
