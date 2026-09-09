4e4bc5d4b72228f38a8b30a7ca3f0e27ccc7a0e3
AGeHt-ua <d.f.12339@gmail.com>
2026-09-09 22:08:37 +0300

    Update accounts.js

--- frontend/assets/accounts.js
+++ frontend/assets/accounts.js
@@ -1,14 +1,7 @@
 window.STATE_ACCOUNTS = [
-  {
-    login: "admin",
-    password: "state-admin",
-    name: "Адміністратор порталу",
-    roles: ["governor"]
-  },
-  {
-    login: "castro",
-    password: "castro",
-    name: "Dominic Castro",
-    roles: ["governor"]
-  }
+  { login: "castro", password: "castro", name: "Dominic Castro", roles: ["governor"] },
+  { login: "admin", password: "state-admin", name: "Адміністратор порталу", roles: ["governor"] },
+  { login: "doj", password: "doj", name: "Attorney General", roles: ["prosecutor"] },
+  { login: "dept", password: "dept", name: "Директор департаменту", roles: ["official"] },
+  { login: "court", password: "court", name: "Голова Верховного Суду", roles: ["court"] }
 ];
